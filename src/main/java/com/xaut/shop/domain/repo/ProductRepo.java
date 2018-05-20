2b6eab2e4a72d53312c42565bb544797946bfb51
king <369558172@qq.com>
2018-05-20 15:57:48 +0800

    feat: Add lock to write product by findById method

--- src/main/java/com/xaut/shop/domain/repo/ProductRepo.java
+++ src/main/java/com/xaut/shop/domain/repo/ProductRepo.java
@@ -2,8 +2,10 @@ package com.xaut.shop.domain.repo;
 
 import com.xaut.shop.domain.entity.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Lock;
 import org.springframework.stereotype.Repository;
 
+import javax.persistence.LockModeType;
 import java.util.List;
 
 @Repository
@@ -11,4 +13,7 @@ public interface ProductRepo extends JpaRepository<Product, Integer> {
     List<Product> findByName(String name);
 
     List<Product> findByNameContainingAndDescriptionContaining(String name, String description);
+
+    @Lock(LockModeType.PESSIMISTIC_WRITE)
+    Product findById(int id);
 }
